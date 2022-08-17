3a64443a95e34cb6923bd96ed96df3d6c40cc0e4
tbangase <ichi.toshi217@gmail.com>
2022-08-17 13:05:40 +0900

    refactor: Arrange directories

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 use dioxus::prelude::*;
 
 fn main() {
-    dioxus::desktop::launch(app);
+    dioxus::web::launch(app);
 }
 
 fn app(cx: Scope) -> Element {
+    let user = Some("Toshiki");
+
     cx.render(rsx!(
-        div { "Hello, World!" }
-        div { "This is second project of Dioxus." }
+        div {
+            h1 { "Hello, ", [if let Some(name) = user { name } else { "World" }], "!" }
+            h2 { onclick: move |_|  println!("h2 clicked"), "This is a Rust app" }
+            ol {
+                li { "Rust" }
+                li { "is" }
+                li { "awesome" }
+            }
+            div { "This is second project of Dioxus." }
+        }
     ))
 }
